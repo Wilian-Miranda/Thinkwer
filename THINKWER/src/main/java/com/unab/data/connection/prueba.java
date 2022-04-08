548753d45211b564fc407d3b2ff35da3ce19c887
Kemberly-Amaya <as0021032020@unab.edu.sv>
2022-04-08 08:37:52 -0600

    Agregando Paquetes

--- THINKWER/src/main/java/com/unab/data/connection/prueba.java
+++ THINKWER/src/main/java/com/unab/data/connection/prueba.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.unab.services;
+package com.unab.data.connection;
 
 /**
  *
  * @author kembe
  */
-public class prueba03 {
+public class prueba {
     
 }
--- THINKWER/src/main/java/com/unab/data/dao/prueba.java
+++ THINKWER/src/main/java/com/unab/data/dao/prueba.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.unab.views;
+package com.unab.data.dao;
 
 /**
  *
  * @author kembe
  */
-public class prueba05 {
+public class prueba {
     
 }
